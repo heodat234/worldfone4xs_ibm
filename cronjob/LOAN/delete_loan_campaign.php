653be17eed2586b18eccd850b2e78ee9e14113e3
heodat234 <thanhhung23495@gmail.com>
2020-02-23 09:39:04 +0700

    hung

--- cronjob/LOAN/delete_loan_campaign.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__DIR__) . "/Header.php";
-use Pheanstalk\Pheanstalk;
-
-$queue = new Pheanstalk('127.0.0.1');
- 
-$mongo_db               = new Mongo_db();
-
-$mongo_db->drop_db("LOAN_campaign_list");
